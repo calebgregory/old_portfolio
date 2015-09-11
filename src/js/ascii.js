ce005ef667291683c5a7fcfe3e8c3bd1abeadfbb
calebgregory <calebgregory@gmail.com>
2015-09-11 02:06:49 -0500

    Making ascii js engine more efficient

--- src/js/ascii.js
+++ src/js/ascii.js
@@ -13,15 +13,14 @@ $(document).ready(() => {
       // Mouse click + moving logic here
 
       if(e.pageY <= currY - 5 && lineHeight < 4.1) {
-        if($(window).width() < $('pre').width()) return;
         lineHeight += 0.1;
+        $('pre').css('line-height',lineHeight);
       } else if(e.pageY > currY + 5 && lineHeight > .3) {
         lineHeight -= 0.1;
+        $('pre').css('line-height',lineHeight);
       }
-      console.log(currY,e.pageY,lineHeight)
       currY = e.pageY;
 
-      $('pre').css('line-height',lineHeight);
     });
 
   });
